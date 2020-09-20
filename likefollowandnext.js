034048b6a691d699ae655b6abc1b4e9155a0360a
venkatesh tripathi <venkateshp.tripathi@gmail.com>
2020-09-20 20:28:25 +0530

    First commit Message

---
+++ likefollowandnext.js
@@ -0,0 +1,5 @@
+var likefollowandnext =setInterval(function(){
+  document.querySelectorAll('.wpO6b')[1].click()
+document.querySelectorAll('.sqdOP')[2].click()
+document.querySelectorAll('._65Bje','.DdSX2')[0].click()
+},8000)
---
+++ likenet.js
@@ -0,0 +1,4 @@
+var likenext=  setInterval(function(){
+  document.querySelectorAll('.wpO6b')[1].click()
+document.querySelectorAll('._65Bje','.DdSX2')[0].click()
+},4000)
